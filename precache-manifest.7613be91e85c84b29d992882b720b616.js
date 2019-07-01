3395862ebf99f8754b9a8d87e36fc6e48505d965
mahesh sharma <abc1@gmail.com>
2019-07-01 17:30:15 +0545

    Updates

---
+++ precache-manifest.7613be91e85c84b29d992882b720b616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78dcb1d6d675ed634c71df27f853345a",
+    "url": "/contactmanager/index.html"
+  },
+  {
+    "revision": "b38fa38c1db613f57081",
+    "url": "/contactmanager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ea6bf5a924c3f8dda3e2",
+    "url": "/contactmanager/static/css/main.f4536ea2.chunk.css"
+  },
+  {
+    "revision": "b38fa38c1db613f57081",
+    "url": "/contactmanager/static/js/2.fe3f2b3f.chunk.js"
+  },
+  {
+    "revision": "ea6bf5a924c3f8dda3e2",
+    "url": "/contactmanager/static/js/main.8643f601.chunk.js"
+  },
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  }
+]);
